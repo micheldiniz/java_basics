92576ef7622333b78139a6c1a8c4054989337f42
Michel Diniz <michel_dinizdasilva@yahoo.com.br>
2023-01-06 16:31:05 -0300

    lecture 20 car dealership project

---
+++ java_basics/src/car_dealership/Dealership.java
@@ -0,0 +1,27 @@
+package car_dealership;
+
+public class Dealership {
+	public static void main(String[] args) {
+		
+		
+		Customer cust1 = new Customer();
+		cust1.setName("Tom");
+		cust1.setAddress("123 Anything St.");;
+		cust1.setCashOnHand(12000);
+		
+		Vehicle vehicle = new Vehicle("Honda", "Accord", 5000);
+		
+		Vehicle car = new Vehicle("Honda", "Accord", 5000);
+		
+		Employee emp = new Employee();
+		
+		cust1.purchaseCar(vehicle, emp, false); 
+		
+		boolean value = car.equals(vehicle);
+		
+		System.out.println(value);
+		
+
+		
+	}
+}
