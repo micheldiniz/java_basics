72e0cec0e49b6f1bca658568c720719804c72229
Michel Diniz <michel_dinizdasilva@yahoo.com.br>
2023-01-18 17:14:59 -0300

    Lecture 36 Starting Threads and the Runnable Interface

--- java_basics/src/com/java_basics/lecture35/threading/Application.java
+++ java_basics/src/com/java_basics/lecture35/threading/Application.java
@@ -6,25 +6,52 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Starting Thread 1");		
+		Thread t1 = new Thread(new Task("Thread-A"));
+		t1.start();		
 		
-		Task taskRunner = new Task();
-		taskRunner.start();
+		System.out.println("Starting Thread 2");		
+		Thread t2 = new Thread(new Task("Thread-B"));
+		t2.start();
 		
-		System.out.println("Hello there!!");
-		
-		
-		Task taskRunner2 = new Task();
-		taskRunner2.start();
-		
+		System.out.println("Starting Thread 3");		
+		Thread t3 = new Thread(new Runnable() {			
+			@Override
+			public void run() {
+				
+				for(int i = 0; i < 10; i++) {
+					System.out.println("number: " + i + " " + Thread.currentThread().getName());
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {			
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		t3.start();	
+
 	}
 	
 }
 
-class Task extends Thread{
+class Task implements Runnable{
+	
+	String name;
+	
+	public Task(String name) {
+		this.name = name;
+	}
 	
 	public void run() {
-		for(int i = 0; i < 1000; i++) {
-			System.out.println("number: " + i);
+		Thread.currentThread().setName(this.name);
+		for(int i = 0; i < 10; i++) {
+			System.out.println("number: " + i + " " + Thread.currentThread().getName());
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {			
+				e.printStackTrace();
+			}
 		}
 	}
 	
