520ce59f1e49de41e412fcedd5123ac8f6c3eb05
Michel Diniz <michel_dinizdasilva@yahoo.com.br>
2023-01-23 11:40:00 -0300

    Lecture 37 Thread Safety Using Synchronization

---
+++ java_basics/src/com/java_basics/lecture37/Sequence.java
@@ -0,0 +1,12 @@
+package com.java_basics.lecture37;
+
+public class Sequence {
+	private int value = 0;
+
+	public synchronized int getNext() {
+//		synchronized (this) {
+			value = value + 1;
+			return value;
+//		}
+	}
+}
