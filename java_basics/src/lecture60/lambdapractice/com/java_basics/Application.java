bcf3f3f5537d9093a8bf34e3bf7993dc4cec4968
Michel Diniz <michel_dinizdasilva@yahoo.com.br>
2023-01-30 15:45:12 -0300

    Lecture 61 Practice with Lambda Expressions (Assignments)

--- java_basics/src/lecture60/lambdapractice/com/java_basics/Application.java
+++ java_basics/src/lecture60/lambdapractice/com/java_basics/Application.java
@@ -24,11 +24,83 @@ public class Application {
 			 System.out.println("Custom object walking... ");
 			 System.out.println("the object tripped... ");
 		 };
-		
+		 
+		 ALambdaInterface helloVar = ()-> {
+			 System.out.println("hello there");
+		 };
+		 
+		 helloVar.someMethod();
+		 Calculate sumVar = (a, b) -> a+b;		 
+		 System.out.println(sumVar.compute(4, 6));
+		 
+		 Calculate nonZeroDivider = (a,b) -> (a==0)?0:a/b;
+		 System.out.println(nonZeroDivider.compute(0, 8));
+		 System.out.println(nonZeroDivider.compute(16, 8));
+		 
+		 MyGenericInterface<String> reverser = (str) -> {
+			 String result = "";
+				for(int i = str.length()-1; i>=0;i--) {
+					result += str.charAt(i);
+				}
+			return result;
+		 };		 
+		 System.out.println(reverser.work("ABCDEFG"));
+		 
+		 MyGenericInterface<Integer> factorial = (num) -> {
+			 int result = 1;
+			 for(int i=1; i<= num; i++) {
+				 result *=i;
+			 }
+			 return result;
+		 };
+		 System.out.println(factorial.work(5));
+		 
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello there");
+	}
+	
+	public int sum(int arg1, int arg2) {
+		return arg1+arg2;
 	}
 	
 	public static void walker(Walkable walkableEntity) {
 		walkableEntity.walk();
 	}
+	
+	public int nonZeroDivide(int arg1, int arg2) {
+		if(arg1==0) {
+			return 0;
+		}
+		return arg1/arg2;
+	}
+	
+	public String reverse(String str){
+		String result = "";
+		for(int i = str.length()-1; i>=0;i--) {
+			result += str.charAt(i);
+		}
+		return result;		
+	}
+	
+	public int factorial(int num) {		
+		return (num==1) ? 1: (num * factorial(num-1));
+	}
 
+}
+interface MyGenericInterface<T>{
+	public T work(T t);
+}
+
+interface Factorial{
+	public int calculate(int a);	
+}
+
+interface StringWorker{
+	public String reverse(String a);
+}
+
+interface Calculate{
+	public int compute(int a, int b);
 }
--- java_basics/src/lecture60/lambdapractice/com/java_basics/Walkable.java
+++ java_basics/src/lecture60/lambdapractice/com/java_basics/Walkable.java
@@ -1,5 +1,7 @@
 package lecture60.lambdapractice.com.java_basics;
 
+//functional interfaces have only one method
+@FunctionalInterface
 public interface Walkable {
 	public void walk();
 }
